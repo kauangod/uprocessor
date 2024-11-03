c934ec2cb776a849da673da75fd68060bb188b7d
Kauan <kkauannunes@gmail.com>
2024-11-03 15:15:04 -0300

    Source e testbench padronizados

--- ula/source/subtractor.vhd
+++ ula/source/subtractor.vhd
@@ -4,12 +4,12 @@ use ieee.numeric_std.all;
 
 entity subtractor is
     port(
-        in0,in1 : in unsigned(15 downto 0);
-        sub     : out unsigned(15 downto 0)
+        in0, in1 : in unsigned(15 downto 0);
+        sub      : out unsigned(15 downto 0)
     );
 end entity;
 
 architecture a_subtractor of subtractor is
 begin
-    sub<=in0-in1;
+    sub <= in0 - in1;
 end architecture;
--- ula/testbench/ULA_tb.vhd
+++ ula/testbench/ULA_tb.vhd
@@ -8,7 +8,7 @@ end entity;
 architecture a_ULA_tb of ULA_tb is
     component ULA
         port(
-            in0,in1                  : in unsigned(15 downto 0);
+            in0, in1                 : in unsigned(15 downto 0);
             sel                      : in unsigned(1 downto 0);
             saida                    : out unsigned(15 downto 0);
             overflow, negative, zero : out std_logic
@@ -19,38 +19,24 @@ architecture a_ULA_tb of ULA_tb is
     signal V, N, Z          : std_logic;
 
     begin
-        uut: ULA port map(
-            in0=>in0,
-            in1=>in1,
-            saida=>saida,
-            sel=>sel,
-            overflow=>V,
-            negative=>N,
-            zero=>Z
-        );
+        uut: ULA 
+            port map(
+                in0 => in0,
+                in1 => in1,
+                saida => saida,
+                sel => sel,
+                overflow => V,
+                negative => N,
+                zero => Z
+            );
     
     process
     begin
-        in0<="0000000001111111";
-        in1<="0001100000011111";
-        sel<="00";
-        wait for 50 ns;
-        in0<="0000000001111111";
-        in1<="0001100000011111";
-        sel<="01";
-        wait for 50 ns;
-        in0<="0001100000011111";
-        in1<="0001100000011111";
-        sel<="01";
-        wait for 50 ns;
-        in0<="0111111111111111";
-        in1<="0111111111111111";
-        sel<="00";
-        wait for 50 ns;
-        in0<="1111111111111000";
-        in1<="0011111111111111";
-        sel<="01";
-        wait for 50 ns;
+        in0 <= "0000000001111111"; in1 <= "0001100000011111"; sel <= "00"; wait for 50 ns;
+        in0 <= "0000000001111111"; in1 <= "0001100000011111"; sel <= "01"; wait for 50 ns;
+        in0 <= "0001100000011111"; in1 <= "0001100000011111"; sel <= "01"; wait for 50 ns;
+        in0 <= "0111111111111111"; in1 <= "0111111111111111"; sel <= "00"; wait for 50 ns;
+        in0 <= "1111111111111000"; in1 <= "0011111111111111"; sel <= "01"; wait for 50 ns;
         wait;
     end process;
 end architecture;
