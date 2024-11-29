9e9484e573274c0423823c73956694269844e785
Kauan <kkauannunes@gmail.com>
2024-11-28 21:57:11 -0300

    Fix ALU instruction and update specific UC conditions

--- top_level/src/rom.vhd
+++ top_level/src/rom.vhd
@@ -16,10 +16,10 @@ architecture a_rom of rom is
   0  => B"0000000101_011_0001", --ld 5 no R3
   1  => B"0000001000_100_0001", --ld 8 no R4
   2  => B"0000_011_100_111_0010", --mov R3 para A
-  3  => B"0000_100_000_111_0100", --add R4 com A
+  3  => B"0000_100_000_111_0010", --add R4 com A
   4  => B"0000_111_100_101_0010", --mov A para R5
   5  => B"0000000001_111_0001", --ld 1 no A
-  6  => B"0000_101_001_111_0100", --sub R5 com A
+  6  => B"0000_101_001_111_0010", --sub R5 com A
   7  => B"0000_111_100_101_0010", --mov A para R5
   8  => B"0010100_000000_0011", --jump para o end 20
   9  => B"0000000000_101_0001", --ld zero no R5
@@ -40,10 +40,10 @@ architecture a_rom of rom is
   );
 
 begin
-  process (clk)
-  begin
-    if (rising_edge(clk)) then
-      data <= rom_content(to_integer(address));
-    end if;
-  end process;
+    process (clk)
+    begin
+      if (rising_edge(clk)) then
+        data <= rom_content(to_integer(address));
+      end if;
+    end process;
 end architecture;
--- uc/src/uc.vhd
+++ uc/src/uc.vhd
@@ -22,63 +22,60 @@ entity UC is
 end entity;
 
 architecture a_UC of UC is
-  component state_machine is
-    port (
-      clk   : in std_logic;
-      reset : in std_logic;
-      state : out unsigned(1 downto 0)
-    );
-  end component;
+    component state_machine is
+      port (
+        clk   : in std_logic;
+        reset : in std_logic;
+        state : out unsigned(1 downto 0)
+      );
+    end component;
 
-  signal opcode  : unsigned(3 downto 0) := (others => '0');
-  signal funct   : unsigned(2 downto 0) := (others => '0');
-  signal state_s : unsigned(1 downto 0) := (others => '0');
+    signal opcode  : unsigned(3 downto 0) := (others => '0');
+    signal funct   : unsigned(2 downto 0) := (others => '0');
+    signal state_s : unsigned(1 downto 0) := (others => '0');
 
 begin
-  st_mach : state_machine
-  port map
-  (
-    clk   => clk,
-    reset => reset,
-    state => state_s
-  );
+    st_mach : state_machine
+    port map
+    (
+      clk   => clk,
+      reset => reset,
+      state => state_s
+    );
 
-  opcode <= instruction(3 downto 0) when state_s = "01" else
-  "0000";
+    opcode <= instruction(3 downto 0);
 
-  funct <= instruction(9 downto 7) when (opcode = "0010" or opcode = "0100") and state_s = "01" else
-  "000";
+    funct <= instruction(9 downto 7) when (opcode = "0010" or opcode = "0100");
 
-  rd <= instruction(6 downto 4) when state_s = "01" else
-  "000";
+    rd <= instruction(6 downto 4);
 
-  rs <= instruction(12 downto 10) when state_s = "01" else
-  "000";
+    rs <= instruction(12 downto 10);
 
-  imm <= (15 downto 10 => instruction(16)) & instruction(16 downto 7) when opcode = "0001" and state_s = "01" else --LD
-         (15 downto 7 => instruction(16)) & instruction(16 downto 10) when opcode = "0011" and state_s = "01" else --Jump
-      "0000000000000000";
+    imm <= (15 downto 10 => instruction(16)) & instruction(16 downto 7) when opcode = "0001" else --LD
+        (15 downto 7 => instruction(16)) & instruction(16 downto 10) when opcode = "0011" else --Jump
+        "0000000000000000";
 
-  ld <= '1' when opcode = "0001" and state_s = "01" else
-  '0';
+    ld <= '1' when opcode = "0001" and state_s = "01" else
+      '0';
 
-  jump <= '1' when opcode = "0011" and state_s = "01" else
-  '0';
+    jump <= '1' when opcode = "0011" and state_s = "01" else
+        '0';
 
-  mov <= '1' when opcode = "0010" and funct = "100" else
-  '0';
+    mov <= '1' when opcode = "0010" and funct = "100" else
+        '0';
 
-  ula_op <= '1' when opcode = "0100" else
-  '0';
+    ula_op <= '1' when opcode = "0010" else
+          '0';
 
-  ula_op_sel <= funct (1 downto 0) when opcode = "0100" else
-  "00";
+    ula_op_sel <= funct (1 downto 0) when opcode = "0010" else
+              "00";
 
-  PC_wr_en <= '1' when state_s = "01" else
-  '0';
+    PC_wr_en <= '1' when state_s = "01" else
+            '0';
 
-  ir_wr_en <= '1' when state_s = "00" else
-  '0';
+    ir_wr_en <= '1' when state_s = "00" else
+            '0';
 
-  state <= state_s;
+    state <= state_s;
+
 end architecture;
